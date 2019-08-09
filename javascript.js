e62419de31a2198ea8eae26cdfd0fd14e6fff4f3
Advaitva <43316329+Advaitva@users.noreply.github.com>
2019-08-09 22:39:47 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -34,6 +34,6 @@ $("input[type='text']").keypress(function(event){
 });
 
 $('.fa-pencil-square-o').on("click",function(event){
-	alert("hello");
+	//alert("hello");
 	$('#hideInput').toggleClass("inputHide");
 })
